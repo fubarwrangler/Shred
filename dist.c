7c6a479688024a7240653bff8cfceb48472c85bb
William Strecker-Kellogg <willsk@bnl.gov>
2013-08-20 14:35:16 -0400

    Handle reading from file and errors properly

--- dist.c
+++ dist.c
@@ -6,14 +6,32 @@ uint64_t arr[256] = {0};
 
 int main(int argc, char *argv[])
 {
+	FILE *fp = stdin;
 	unsigned char buf[4096];
-	uint64_t max = 0, norm = 0;
+	uint64_t total = 0;
+	uint64_t norm = 0;
+	uint64_t max = 0;
 	size_t n, i;
 
-	while((n = fread(buf, 1, sizeof(buf), stdin)) != 0)
+	if(argc > 1)	{
+		if((fp = fopen(argv[1], "r")) == NULL)	{
+			fprintf(stderr, "Error opening %s\n", argv[1]);
+			return 1;
+		}
+	}
+
+	while((n = fread(buf, 1, sizeof(buf), fp)) != 0)	{
 		for(i = 0; i < n; i++)
 			arr[buf[i]]++;
+		total += n;
+	}
 
+	if(ferror(fp))	{
+		fprintf(stderr, "File error occured\n");
+		return 1;
+	}
+
+	fclose(fp);
 
 	for(i = 0; i < 256; i++)	{
 		if(arr[i] > max)
@@ -33,7 +51,7 @@ int main(int argc, char *argv[])
 		}
 		printf("\n");
 	}
-
+	printf("%lu bytes read total\n", total);
 
 	return 0;
 }
