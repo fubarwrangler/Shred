ab4ec71b338ea99cf4556309938425df82efacb6
William Strecker-Kellogg <willsk@bnl.gov>
2013-10-31 15:59:15 -0400

    WIP Make dist able to display in base 2, 16, or 256

--- dist.c
+++ dist.c
@@ -6,9 +6,76 @@
  ************************************************************************/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
 
+#include "cmdlineparse.h"
+
+enum {
+	BIT = 1,
+	HEX = 4,
+	BYTE = 8,
+};
 
 uint64_t arr[256] = {0};
+char bitcount = BYTE;
+char *file = NULL;
+
+/* Set the configuration options above from cmdline */
+static void initialize_options(int argc, char *argv[])
+{
+	int c;
+
+	while((c=getopt(argc, argv, "+hb:")) != -1) {
+		switch(c)   {
+			case 'b':
+				switch(bitcount = parse_num(c))	{
+					case 1:
+						bitcount = BIT;
+						break;
+					case 4:
+						bitcount = HEX;
+						break;
+					case 8:
+						bitcount = BYTE;
+						break;
+					default:
+						fprintf(stderr, "Error, bit count needs to be '1', '4', or '8'\n");
+						exit(EXIT_FAILURE);
+				}
+				break;
+			case 'h':
+				fprintf(stderr,
+"Usage: %s [options] [FILE] \n\
+	FILE	file or device to read (default is stdin)\n\
+  Options:\n\
+	-b  Bit-length, valid values are 1, 4, and 8\n\
+", argv[0]);
+				exit(EXIT_SUCCESS);
+			case '?':
+				if(strchr("bh", optopt) == NULL)
+					fprintf(stderr,
+						"Unknown option -%c encountered\n", optopt);
+				else
+					fprintf(stderr,
+						"Option -%c requires an argument\n", optopt);
+				exit(EXIT_FAILURE);
+			default:
+				abort();
+		}
+	}
+
+	if(optind + 1 == argc)	{
+		file = argv[optind];
+		return;
+	} else if (optind + 1 < argc) {
+		fprintf(stderr, "Error, extra arguments found starting with: %s\n",
+				argv[optind + 1]);
+		exit(EXIT_FAILURE);
+	}
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,7 +86,9 @@ int main(int argc, char *argv[])
 	uint64_t max = 0;
 	size_t n, i;
 
-	if(argc > 1)	{
+	initialize_options(argc, argv);
+
+	if(file != NULL)	{
 		if((fp = fopen(argv[1], "r")) == NULL)	{
 			fprintf(stderr, "Error opening %s\n", argv[1]);
 			return 1;
@@ -48,7 +117,7 @@ int main(int argc, char *argv[])
 	if(max == 0)
 		return 1;
 
-	for(int j = 0; j < 256; j++)	{
+	for(int j = 0; j < (1 << bitcount); j++)	{
 		int stat;
 		stat = (60 * arr[j]) / max;
 		printf("%-2x (%7.3f%%) |", j, 100.0f * (double)arr[j] / (double)norm);
